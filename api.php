224b3db495f683b326491e7d18d9cbeae64f7b78
AnVolkova10 <curzi.angela@gmail.com>
2021-07-03 17:59:24 -0300

    probando api

--- api.php
+++ api.php
@@ -6,6 +6,27 @@ echo '<pre>';
 $api_response = file_get_contents('https://valorant-api.com/v1/agents');
 
 
-$valorant = json_decode($api_response, true);
+if ($api_response !== false) {
+    $agents = json_decode($api_response, true);
 
-var_dump($valorant);
+    if ( !empty ($agents['status'])) {
+        echo '<ul>';
+        foreach ($agents["data"] as $agent_data => $description) {
+            if ( empty($agents["data"][$agent_data]["role"]["displayName"])  ) {            
+            } else {
+            echo $agents["data"][$agent_data]["displayName"] . '<br>';            //trae nombre
+            echo $agents["data"][$agent_data]["description"] . '<br>';              //trae descripción
+            echo $agents["data"][$agent_data]["role"]["displayName"] . '<br>';      //trae clase
+            '<hr>' ;
+        }
+        }
+
+        echo '</ul>';
+    } 
+}
+var_dump($agents);
+die();
+
+
+
+
