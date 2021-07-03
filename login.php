efe841338eff8bd81a94c0b79249c514c4b75009
AnVolkova10 <curzi.angela@gmail.com>
2021-07-03 13:59:38 -0300

    1/2 login php

--- login.php
+++ login.php
@@ -2,14 +2,22 @@
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
+$user = '';
+$password = '';
+
+if ( !empty ($_POST)) {
+    if ( !empty ($_POST['user']) && is_string ($_POST['user'])){
+        $user = $_POST['user'];
+    }
+    if ( !empty ($_POST['password']) && is_string ($_POST['password'])){
+        $password = $_POST['password'];
+    }
+}
 
 
+if ($user !=='' && $password !=='') {
 
-
-
-
-
-
+}
 
 
 
